24a39868486d7b0dc87a66f0307134968ca9a510
Henning <henninglulei@gmail.com>
2022-07-08 10:37:49 +0200

    changed location of language selection

--- ui.py
+++ ui.py
@@ -5,28 +5,28 @@ from translation import translate
 
 st.title('EWBot für die EWBerichtserstattung')
 
-do_translation = st.checkbox('Auf deutsch übersetzen')
 load = st.checkbox('Vollständige Artikel laden')
 
 articles = search(end_date=st.date_input('Enddatum'))
 categories = {a.category for a in articles}
 
 with st.sidebar:
-    st.header('Kategorien')
-    if not do_translation:
+    language = st.radio('Sprache der Artikel', ('englisch', 'deutsch'))
+    st.subheader('Kategorien')
+    if language == 'englisch':
         checkboxes = {c: st.checkbox(c, value=True) for c in categories}
-    else:
+    elif language == 'deutsch':
         checkboxes = {c: st.checkbox(translate(c), value=True) for c in categories}
 
 for article in articles:
     if checkboxes[article.category]:
-        if not do_translation:
+        if language == 'englisch':
             st.markdown(article.to_html(), unsafe_allow_html=True)
             if load:
                 with st.expander('Vollständigen Artikel lesen'):
                     st.markdown(article.full_article, unsafe_allow_html=True)
 
-        else:
+        elif language == 'deutsch':
             st.markdown(article.german_version.to_html(), unsafe_allow_html=True)
             if load:
                 with st.expander('Vollständigen Artikel lesen'):
