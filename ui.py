26dd3b57b5934aa227a4f9a0e970370befbc1a72
Henning <henninglulei@gmail.com>
2022-07-11 22:40:15 +0200

    added link to one-note document

--- ui.py
+++ ui.py
@@ -13,6 +13,7 @@ except ReadError:
 
 
 st.title('EWBot für die EWBerichtserstattung')
+st.markdown('<a href="https://ewbkarlsruhe.sharepoint.com/sites/haiti-teamorder/_layouts/15/Doc.aspx?sourcedoc={6f989814-dbf5-46bd-96c7-d55795fe6b41}&action=edit&wd=target%28News.one%7C18eaa96c-23d1-426b-88f0-1689d9042f7f%2FNews-Update%7C973fe869-1c36-9144-8671-63fd5bda7db8%2F%29">One-Note Dokument</a>', unsafe_allow_html=True)
 
 load = st.checkbox('Vollständige Artikel laden')
 
@@ -20,10 +21,11 @@ load = st.checkbox('Vollständige Artikel laden')
 with st.sidebar:
     language = st.radio('Sprache der Artikel', ('englisch', 'deutsch'))
     st.subheader('Kategorien')
+    checkboxes = {}
     if language == 'englisch':
-        checkboxes = {c[0]: st.checkbox(c[0], value=True, key=c[0]) for c in categories}
+        checkboxes.update({c[0]: st.checkbox(c[0], value=True, key=c[0]) for c in categories})
     elif language == 'deutsch':
-        checkboxes = {c[0]: st.checkbox(c[1], value=True, key=c[1]) for c in categories}
+        checkboxes.update({c[0]: st.checkbox(c[1], value=True, key=c[1]) for c in categories})
 
 for article in articles:
     if checkboxes[article.category]:
